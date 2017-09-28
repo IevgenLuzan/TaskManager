4116e6e82ddcf95ac76e2e521e155703fb827223
IevgenLuzan <evgenluzan@gmail.com>
2017-09-28 19:31:24 +0300

    laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home_admin', 'HomeController@home');
+Route::get('/home_user', 'HomeController@home');
+Route::get('/task_user', 'TaskController@store');
+Route::get('/edit', 'TaskController@edit');
+Route::delete('/task/{task}', 'TaskController@destroy');
+Route::post('/task/add', 'TaskController@add');
+Route::post('/task/edit', 'TaskController@edit');      
+        
