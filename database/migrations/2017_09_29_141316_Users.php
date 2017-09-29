79ecf6e462d025d782b894a26bc79f17bbbe0eb6
sasha44 <12345@i.ua>
2017-09-29 18:05:16 +0300

    filed all migrations

--- database/migrations/2017_09_29_141316_Users.php
+++ database/migrations/2017_09_29_141316_Users.php
@@ -1,18 +1,22 @@
 <?php
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Users extends Migration
-{
+class Users extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        //
+    public function up() {
+        Schema::create('Users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->varchar('name');
+            $table->enum('role', ['teacher', 'student']);
+            //$table->timestamps();
+        });
     }
 
     /**
@@ -20,8 +24,8 @@ class Users extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        //
+    public function down() {
+       Schema::drop('Users');
     }
+
 }
--- database/migrations/2017_09_29_141406_Tasks.php
+++ database/migrations/2017_09_29_141406_Tasks.php
@@ -1,18 +1,22 @@
 <?php
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Tasks extends Migration
-{
+class Tasks extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        //
+    public function up() {
+        Schema::create('Tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->varchar('name');
+            $table->string('description');
+            //$table->timestamps();
+        });
     }
 
     /**
@@ -20,8 +24,8 @@ class Tasks extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        //
+    public function down() {
+        Schema::drop('Tasks');
     }
+
 }
