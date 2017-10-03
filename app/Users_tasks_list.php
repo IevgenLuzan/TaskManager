9b6d3ae7bb08d371fd6510676a66a9a0c18d8d27
IevgenLuzan <evgenluzan@gmail.com>
2017-10-03 21:52:56 +0300

    Test routs

--- app/Users_tasks_list.php
+++ app/Users_tasks_list.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Users_tasks_list extends Model
+class Users_tasks_lists extends Model
 {
-    //
+    protected $fillable = ['user_id', 'task_id', 'condition'];
 }
